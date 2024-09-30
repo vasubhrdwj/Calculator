9a48e52e2bc78a00b8b428bc17abdae7b04aab21
Vasu Bhardwaj <vasubhrdwj@gmail.com>
2024-10-01 00:18:44 +0530

    Added variables for readabiltiy

--- CalculatorJS.js
+++ CalculatorJS.js
@@ -14,9 +14,14 @@ function Divide(a, b) {
   return a / b;
 }
 
-let a = 18;
-let b = 6;
-console.log(`Sum of ${a} and ${b} is ${Add(a, b)}`);
-console.log(`Difference of ${a} and ${b} is ${Subtract(a, b)}`);
-console.log(`Multiplication of ${a} and ${b} is ${Multiply(a, b)}`);
-console.log(`Division of ${a} and ${b} is ${Divide(a, b)}`);
+let num1 = 18;
+let num2 = 6;
+let oper = "+";
+
+console.log(`${num1} ${oper} ${num2} = ${Add(num1, num2)}`);
+oper = "-";
+console.log(`${num1} ${oper} ${num2} = ${Subtract(num1, num2)}`);
+oper = "*";
+console.log(`${num1} ${oper} ${num2} = ${Multiply(num1, num2)}`);
+oper = "/";
+console.log(`${num1} ${oper} ${num2} = ${Divide(num1, num2)}`);
